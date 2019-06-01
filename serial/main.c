7de9403b7b905d35520c8f2326e31e2737193dfa
JiuSenso <sensoliniarra.1661198@studenti.uniroma1.it>
2019-06-01 11:47:18 +0200

    adjust indentation

--- serial/main.c
+++ serial/main.c
@@ -75,7 +75,7 @@ int main(int argc, char* argv[]){
   bool running = true;
   sem_init(&mutex, 0, 1);
 
-  uint8_t buf_send[4]={OS_FLAG,5,CWISE,0}, buf_rcv[4]={0xFF,0xFF,0xFF,0};
+  uint8_t buf_send[4]={OS_FLAG,25,CWISE,0}, buf_rcv[4]={0xFF,0xFF,0xFF,0};
 
   packet_t packet_send, packet_rcv;
   memcpy(&packet_send, buf_send, 3);
@@ -107,122 +107,124 @@ int main(int argc, char* argv[]){
                         listenerRoutine,
                         (void*)&params);                        
   if( ret != 0 ){
-    perror("Error: cannot create Listener Thread!\n");
+    perror("Error: cannot create listener thread!\n");
     exit(EXIT_FAILURE);
   }
-  else printf("# Listener Thread successfully created.\n");
+  else printf("# Listener thread successfully created.\n");
 
-    //create GUI ------------------------------------------
-    WINDOW *menu_win, *sender_win;
-	  initGUI();
-    printWelcomeMessage(welcome_msg);
+  //create GUI ------------------------------------------
+  WINDOW *menu_win, *sender_win;
+  printf("# initializing GUI...\n");
+	initGUI();
+  printWelcomeMessage(welcome_msg);
 
-    int x_max=1, y_max=1;
-    getmaxyx(stdscr, y_max, x_max); //this is a macro
+  int x_max=1, y_max=1;
+  getmaxyx(stdscr, y_max, x_max); //this is a macro
 
-    //initialize windows (size_y, size_x, pos_y, pos_x)
-    menu_win = newwin(8, MENU_BOX_WIDTH, MENU_YPOS, MENU_XPOS);
-    sender_win = newwin(6, 30, MENU_YPOS+1, MENU_XPOS+16);
+  //initialize windows (size_y, size_x, pos_y, pos_x)
+  menu_win = newwin(8, MENU_BOX_WIDTH, MENU_YPOS, MENU_XPOS);
+  sender_win = newwin(6, 30, MENU_YPOS+1, MENU_XPOS+16);
 
-    box(menu_win, 0, 0);
-    mvwprintw(menu_win, 0, MENU_BOX_WIDTH/2-4, " SETTINGS ");
-    printSender(sender_win, packet_send, speed_str, dir_str, refresh_str);
-    keypad(menu_win, true);
+  box(menu_win, 0, 0);
+  mvwprintw(menu_win, 0, MENU_BOX_WIDTH/2-4, " SETTINGS ");
+  printSender(sender_win, packet_send, speed_str, dir_str, refresh_str);
+  keypad(menu_win, true);
 
-    //refresh everything ----------------------------------
-    refresh();
-    wrefresh(menu_win);
-    wrefresh(sender_win);
-    clear();
+  //refresh everything ----------------------------------
+  refresh();
+  wrefresh(menu_win);
+  wrefresh(sender_win);
+  clear();
 
-    bool something_change = true;
-    running = true;
-    sem_post(&mutex); //end critical section
+  bool something_change = true;
+  running = true;
+  sem_post(&mutex); //end critical section
 
 /*::::::::: SENDER LOOP :::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-    while( running ){
+  while( running ){
       
-      if( something_change ) {
-        if( !writePacket(fd, &packet_send) ){
-          endwin();
-          printf("Error: bytes send should be %lu\n", sizeof(buf_send));
-          exit(EXIT_FAILURE);
-        }
-        something_change = false;
-        nanosleep(&ts, &ts_rem);  //remove?
+    if( something_change ) {
+      if( !writePacket(fd, &packet_send) ){
+        endwin();
+        printf("Error: bytes send should be %lu\n", sizeof(buf_send));
+        exit(EXIT_FAILURE);
       }
-
-
-      for(int i=0 ; i<NUM_CHOICES ; i++){
-        if(i == highlight) wattron(menu_win, A_REVERSE | A_BOLD);
-        mvwprintw(menu_win, i+2, 3, choices[i]);
-        wattroff(menu_win, A_REVERSE | A_BOLD);
-      }
-
-      choice = wgetch(menu_win);  //blocking
-      switch(choice){
-
-        case KEY_UP:
-          highlight--;
-          if(highlight == -1) highlight = NUM_CHOICES-1;
-          break;
-
-        case KEY_DOWN:
-          highlight++;
-          if(highlight == NUM_CHOICES) highlight = 0;
-          break;
-
-        case KEY_LEFT:
-          if( highlight == 0 ){
-            something_change = decreaseSpeed(&packet_send);
-            createSpeedString(speed_str, packet_send.speed);
-          }
-          if( highlight == 1 ){
-            something_change = changeDirection(&packet_send);
-            createDirectionString(dir_str, packet_send.direction);
-          }
-          if( highlight == 2 ){
-            something_change = decreaseRefreshRate(&packet_send);
-            createPacketRateString(refresh_str, packet_send.timestamp);
-          }
-          break;
-
-        case KEY_RIGHT:
-          if( highlight == 0 ){
-            something_change = increaseSpeed(&packet_send);
-            createSpeedString(speed_str, packet_send.speed);
-          }
-          if( highlight == 1 ){
-            something_change = changeDirection(&packet_send);
-            createDirectionString(dir_str, packet_send.direction);
-          }
-          if( highlight == 2 ){
-            something_change = increaseRefreshRate(&packet_send);
-            createPacketRateString(refresh_str, packet_send.timestamp);
-          }
-          break;
-
-        case 10:  //CASE 'ENTER'
-          if( highlight == NUM_CHOICES-1 ){
-            running = false;
-          }
-          break;
-
-        default:
-          break;
-		  }
-
-      printSender(sender_win, packet_send, speed_str, dir_str, refresh_str);
-      wrefresh(sender_win);
+      something_change = false;
+      nanosleep(&ts, &ts_rem);  //remove?
     }
+
+
+    for(int i=0 ; i<NUM_CHOICES ; i++){
+      if(i == highlight) wattron(menu_win, A_REVERSE | A_BOLD);
+      mvwprintw(menu_win, i+2, 3, choices[i]);
+      wattroff(menu_win, A_REVERSE | A_BOLD);
+    }
+
+    choice = wgetch(menu_win);  //blocking
+    switch(choice){
+
+      case KEY_UP:
+        highlight--;
+        if(highlight == -1) highlight = NUM_CHOICES-1;
+        break;
+
+      case KEY_DOWN:
+        highlight++;
+        if(highlight == NUM_CHOICES) highlight = 0;
+        break;
+
+      case KEY_LEFT:
+        if( highlight == 0 ){
+          something_change = decreaseSpeed(&packet_send);
+          createSpeedString(speed_str, packet_send.speed);
+        }
+        if( highlight == 1 ){
+          something_change = changeDirection(&packet_send);
+          createDirectionString(dir_str, packet_send.direction);
+        }
+        if( highlight == 2 ){
+          something_change = decreaseRefreshRate(&packet_send);
+          createPacketRateString(refresh_str, packet_send.timestamp);
+        }
+        break;
+
+      case KEY_RIGHT:
+        if( highlight == 0 ){
+          something_change = increaseSpeed(&packet_send);
+          createSpeedString(speed_str, packet_send.speed);
+        }
+        if( highlight == 1 ){
+          something_change = changeDirection(&packet_send);
+          createDirectionString(dir_str, packet_send.direction);
+        }
+        if( highlight == 2 ){
+          something_change = increaseRefreshRate(&packet_send);
+          createPacketRateString(refresh_str, packet_send.timestamp);
+        }
+        break;
+
+      case 10:  //CASE 'ENTER'
+        if( highlight == NUM_CHOICES-1 ){
+          running = false;
+        }
+        break;
+
+      default:
+        break;
+		}
+
+    printSender(sender_win, packet_send, speed_str, dir_str, refresh_str);
+    wrefresh(sender_win);
+  }
 
 /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 
-    pthread_join(listener_thread, NULL);
-    sem_destroy(&mutex);
-    closeSerialCommunication(&fd);
-    endwin();
-    printf("# Closing serial communication... Done.\n");
-
-    return 0;
+  pthread_join(listener_thread, NULL);
+  sem_destroy(&mutex);
+  closeSerialCommunication(&fd);
+  endwin();
+  printf("# Serial communication closed...\n# exit...\n");
+  nanosleep(&ts, &ts_rem);
+  
+  return 0;
 }
